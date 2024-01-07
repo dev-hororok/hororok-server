990f4482b157bb1c27a90d47e5cdaa6f715059cc
woo3145 <lcwoo3145@gmail.com>
2024-01-07 19:18:29 +0900

    ✨ 응답 형식 통일(Exception, Interceptor)

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { RolesGuard } from './auth/guard/roles.guard';
+import { JwtAuthGuard } from './auth/guard/jwt-auth.guard';
+import { HttpExceptionFilter } from './common/exceptions/http-exception/http-exception.filter';
+import { SuccessInterceptor } from './common/interceptors/success/success.interceptor';
 
 @Module({
   imports: [
@@ -20,9 +23,22 @@ import { RolesGuard } from './auth/guard/roles.guard';
   controllers: [AppController],
   providers: [
     AppService,
+    // 실험결과 APP_GUARD도 위부터 아래로 순서가 보장됨
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
     {
       provide: APP_GUARD,
       useClass: RolesGuard,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: SuccessInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
     },
   ],
 })
