a57db1fed0eb3d52404f52b1b06b980da9a9f18c
woo3145 <lcwoo3145@gmail.com>
2024-01-22 16:43:16 +0900

    ♻️ exists 메소드 제거

--- src/accounts/accounts.repository.ts
+++ src/accounts/accounts.repository.ts
@@ -1,6 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { FilterQuery, Model } from 'mongoose';
-import { Account, AccountDocument } from './entities/account.model';
+import { Model } from 'mongoose';
+import { AccountDocument } from './entities/account.model';
 import { EntityRepository } from '@src/database/entity.repository';
 
 @Injectable()
@@ -10,11 +10,5 @@ export class AccountsRepository extends EntityRepository<AccountDocument> {
     private readonly accountModel: Model<AccountDocument>,
   ) {
     super(accountModel);
-  }
-
-  async exists(
-    accountFilterQuery: FilterQuery<Account>,
-  ): Promise<{ _id: any }> {
-    return this.accountModel.exists(accountFilterQuery);
   }
 }
--- src/accounts/accounts.service.ts
+++ src/accounts/accounts.service.ts
@@ -8,23 +8,23 @@ import { AccountsRepository } from './accounts.repository';
 import { v4 as uuid } from 'uuid';
 import * as crypto from 'crypto';
 import { Role } from './entities/role.enum';
+import { Account } from './entities/account.model';
 
 @Injectable()
 export class AccountsService {
   constructor(private readonly accountsRepository: AccountsRepository) {}
 
-  async findOneById(id: string) {
+  async findOneById(id: string): Promise<Account> {
     const account = await this.accountsRepository.findOne({ account_id: id });
     return account;
   }
-  async findOneByEmail(email: string) {
+  async findOneByEmail(email: string): Promise<Account> {
     const account = await this.accountsRepository.findOne({ email });
     return account;
   }
 
-  async create({ email, password }: CreateAccountDto) {
-    const exist = await this.accountsRepository.exists({ email });
-    console.log(exist);
+  async create({ email, password }: CreateAccountDto): Promise<Account> {
+    const exist = await this.accountsRepository.findOne({ email });
     if (exist) {
       throw new BadRequestException('이미 가입된 이메일입니다.');
     }
@@ -40,7 +40,10 @@ export class AccountsService {
     });
   }
 
-  async changePassword(account_id: string, { password }: ChangePasswordDto) {
+  async changePassword(
+    account_id: string,
+    { password }: ChangePasswordDto,
+  ): Promise<Account> {
     const hashedPassword = await bcrypt.hash(password, 10);
     const updatedAccount = await this.accountsRepository.findOneAndUpdate(
       { account_id },
@@ -54,7 +57,10 @@ export class AccountsService {
     return updatedAccount;
   }
 
-  async update(account_id: string, editAccountDto: EditAccountDto) {
+  async update(
+    account_id: string,
+    editAccountDto: EditAccountDto,
+  ): Promise<Account> {
     const updatedAccount = await this.accountsRepository.findOneAndUpdate(
       { account_id },
       editAccountDto,
