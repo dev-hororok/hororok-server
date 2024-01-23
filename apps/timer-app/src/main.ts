22b6a7efff39fae26e3f63a5ab07c2e8b8ff3bed
woo3145 <lcwoo3145@gmail.com>
2024-01-24 02:12:16 +0900

    ✨ 모노레포 - filter 공동 모듈화

--- apps/timer-app/src/main.ts
+++ apps/timer-app/src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { TimerAppModule } from './timer-app.module';
-import { Interceptor } from '@app/config';
+import { CustomExceptionFilter, Interceptor } from '@app/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(TimerAppModule);
 
   app.useGlobalInterceptors(new Interceptor());
+  app.useGlobalFilters(new CustomExceptionFilter());
 
   const PORT = process.env.TIMER_APP_PORT || 4001;
   await app.listen(PORT);
