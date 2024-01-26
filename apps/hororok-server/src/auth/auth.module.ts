6ce975cb6480219587534b5b75e4aa78851b7f2a
woo3145 <lcwoo3145@gmail.com>
2024-01-27 05:18:48 +0900

    💚 🐛 Hororok - Auth모듈 의존성 수정

--- apps/hororok-server/src/auth/auth.module.ts
+++ apps/hororok-server/src/auth/auth.module.ts
@@ -3,11 +3,10 @@ import { AuthService } from './services/auth.service';
 import { AuthController } from './controllers/auth.controller';
 import { LocalStrategy } from './strategies/local.strategy';
 import { AccountsModule } from '../accounts/accounts.module';
-import { JwtModule } from '@nestjs/jwt';
 import { SharedAuthModule } from '@app/auth';
 
 @Module({
-  imports: [AccountsModule, SharedAuthModule, JwtModule],
+  imports: [AccountsModule, SharedAuthModule],
   providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
 })
