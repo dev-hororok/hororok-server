c129b4752b3c5ee1c7262baf904afe008a8ba502
woo3145 <lcwoo3145@gmail.com>
2024-01-27 04:37:48 +0900

    🐛 배포환경 버그 수정

--- apps/hororok-server/src/app.module.ts
+++ apps/hororok-server/src/app.module.ts
@@ -4,13 +4,14 @@ import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { AccountsModule } from './accounts/accounts.module';
 import { AppController } from './app.controller';
-import { JwtAuthGuard, RolesGuard } from '@app/auth';
+import { JwtAuthGuard, RolesGuard, SharedAuthModule } from '@app/auth';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    SharedAuthModule,
     AuthModule,
     AccountsModule,
   ],
--- apps/hororok-server/src/auth/auth.module.ts
+++ apps/hororok-server/src/auth/auth.module.ts
@@ -3,11 +3,10 @@ import { AuthService } from './services/auth.service';
 import { AuthController } from './controllers/auth.controller';
 import { LocalStrategy } from './strategies/local.strategy';
 import { AccountsModule } from '../accounts/accounts.module';
-import { SharedAuthModule } from '@app/auth';
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [AccountsModule, SharedAuthModule, JwtModule.register({})],
+  imports: [AccountsModule, JwtModule.register({})],
   providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
 })
