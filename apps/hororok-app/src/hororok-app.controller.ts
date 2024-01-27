560a1c5874e5deb30bded7e2b21daea54689c16c
woo3145 <lcwoo3145@gmail.com>
2024-01-27 20:18:39 +0900

    ✨ Hororok-App 공유모듈 DI

--- apps/hororok-app/src/hororok-app.controller.ts
+++ apps/hororok-app/src/hororok-app.controller.ts
@@ -1,15 +1,18 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { HororokAppService } from './hororok-app.service';
+import { Public } from '@app/auth';
 
 @Controller()
 export class HororokAppController {
   constructor(private readonly hororokAppService: HororokAppService) {}
 
+  @Public()
   @Get()
   getHello(): string {
     return this.hororokAppService.getHello();
   }
 
+  @Public()
   @Post()
   test(@Body() body) {
     console.log(body);
--- apps/hororok-app/src/hororok-app.module.ts
+++ apps/hororok-app/src/hororok-app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { HororokAppController } from './hororok-app.controller';
 import { HororokAppService } from './hororok-app.service';
 import { APP_GUARD } from '@nestjs/core';
-import { JwtAuthGuard, RolesGuard } from '@app/auth';
+import { JwtAuthGuard, RolesGuard, SharedAuthModule } from '@app/auth';
+import { DatabaseModule } from '@app/database';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    SharedAuthModule,
+  ],
   controllers: [HororokAppController],
   providers: [
     HororokAppService,
