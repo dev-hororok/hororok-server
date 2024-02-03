380d2f3c3d21240027e8c39158ff701ed4751291
woo3145 <lcwoo3145@gmail.com>
2024-02-03 18:58:18 +0900

    ✨ 트랜잭션 처리(StudyRecord, StudyCategory)

--- apps/timer-app/src/study-categories/study-categories.service.ts
+++ apps/timer-app/src/study-categories/study-categories.service.ts
@@ -1,93 +1,110 @@
 import { StudyCategory } from '@app/database/typeorm/entities/study-category.entity';
-import { BadRequestException, Injectable } from '@nestjs/common';
-import { In, Repository } from 'typeorm';
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  forwardRef,
+} from '@nestjs/common';
+import {
+  DataSource,
+  FindManyOptions,
+  FindOneOptions,
+  QueryRunner,
+  Repository,
+} from 'typeorm';
 import { CreateStudyCategoryInputDto } from './dtos/create-study-category.dto';
 import { UpdateStudyCategoryInputDto } from './dtos/update-study-category.dto';
-import { StudyRecord } from '@app/database/typeorm/entities/study-record.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { StudyRecordsService } from '../study-records/study-records.service';
 
 @Injectable()
 export class StudyCategoriesService {
   constructor(
     @InjectRepository(StudyCategory)
     private studyCategoryRepository: Repository<StudyCategory>,
-    @InjectRepository(StudyRecord)
-    private studyRecordRepository: Repository<StudyRecord>,
+    @Inject(forwardRef(() => StudyRecordsService))
+    private studyRecordsService: StudyRecordsService,
+    private dataSource: DataSource,
   ) {}
 
-  async create(member_id: string, { subject }: CreateStudyCategoryInputDto) {
-    const exist = await this.studyCategoryRepository.findOne({
-      where: {
-        member: { member_id: member_id },
-        subject,
-      },
-    });
-    if (exist && !exist.is_hidden) {
-      throw new BadRequestException('이미 동일한 카테고리가 존재합니다.');
-    }
-    // 만들어진 이력이 있으면 살려서 반환
-    if (exist) {
-      exist.is_hidden = false;
-      const result = await this.studyCategoryRepository.save(exist);
-      return result;
-    }
-
-    // 없으면 새로 생성
-    const newCategory = this.studyCategoryRepository.create({
-      member: {
-        member_id,
-      },
-      subject,
-      is_hidden: false,
-    });
-
-    const result = await this.studyCategoryRepository.save(newCategory);
-    return result;
-  }
-
-  private async findOneByMemberIdAndSubject(
-    member_id: string,
-    subject: string,
+  async findAll(
+    options: FindManyOptions<StudyCategory>,
+    queryRunner?: QueryRunner,
   ) {
-    return this.studyCategoryRepository.findOne({
-      where: { subject, member: { member_id } },
-    });
+    if (queryRunner) {
+      return queryRunner.manager.getRepository(StudyCategory).find(options);
+    }
+    return this.studyCategoryRepository.find(options);
   }
 
-  async findOneById(category_id: number) {
-    return this.studyCategoryRepository.findOne({
-      where: { study_category_id: category_id },
-      relations: ['member'],
-    });
-  }
-
-  async findByMemberId(member_id: string) {
-    const categories = await this.studyCategoryRepository.find({
-      where: {
-        member: {
-          member_id,
-        },
-        is_hidden: false,
-      },
-    });
-    return categories;
+  async findOne(
+    options: FindOneOptions<StudyCategory>,
+    queryRunner?: QueryRunner,
+  ) {
+    if (queryRunner) {
+      return queryRunner.manager.getRepository(StudyCategory).findOne(options);
+    }
+    return this.studyCategoryRepository.findOne(options);
   }
 
   async delete(study_category_id: number) {
-    const exist = await this.studyCategoryRepository.findOne({
-      where: {
-        study_category_id,
-      },
-    });
-    if (!exist) {
-      throw new BadRequestException('해당 카테고리가 존재하지 않습니다.');
-    }
-    if (exist.is_hidden) {
-      throw new BadRequestException('이미 삭제된 카테고리입니다.');
-    }
-    exist.is_hidden = true;
+    const result =
+      await this.studyCategoryRepository.softDelete(study_category_id);
+    return result.affected ? 0 < result.affected : false;
+  }
 
-    return this.studyCategoryRepository.save(exist);
+  async create(member_id: string, { subject }: CreateStudyCategoryInputDto) {
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      const exist = await this.findOne(
+        {
+          withDeleted: true,
+          where: {
+            member: { member_id: member_id },
+            subject,
+          },
+        },
+        queryRunner,
+      );
+      if (exist && !exist.deleted_at) {
+        throw new BadRequestException('이미 동일한 카테고리가 존재합니다.');
+      }
+      // 만들어진 이력이 있으면 살려서 반환
+      if (exist && exist.deleted_at) {
+        await queryRunner.manager.update(
+          StudyCategory,
+          exist.study_category_id,
+          {
+            deleted_at: null,
+          },
+        );
+        await queryRunner.commitTransaction();
+        exist.deleted_at = null;
+        return exist;
+      }
+
+      // 없으면 새로 생성
+      const newCategory = queryRunner.manager.create(StudyCategory, {
+        member: {
+          member_id,
+        },
+        subject,
+      });
+
+      await queryRunner.manager.insert(StudyCategory, newCategory);
+      await queryRunner.commitTransaction();
+
+      return newCategory;
+    } catch (e) {
+      await queryRunner.rollbackTransaction();
+      throw e;
+    } finally {
+      await queryRunner.release();
+    }
   }
 
   async update(
@@ -95,41 +112,59 @@ export class StudyCategoriesService {
     study_category_id: number,
     { subject }: UpdateStudyCategoryInputDto,
   ) {
-    const existingCategory =
-      await this.findCategoryWithRecords(study_category_id);
+    const queryRunner = this.dataSource.createQueryRunner();
 
-    this.validateCategoryExistence(existingCategory);
-    this.validateSubjectChange(existingCategory, subject);
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
 
-    const targetCategory = await this.findOneByMemberIdAndSubject(
-      member_id,
-      subject,
-    );
+    try {
+      const existingCategory = await this.findOne(
+        {
+          where: {
+            study_category_id,
+          },
+          relations: {
+            study_records: true,
+          },
+        },
+        queryRunner,
+      );
+      if (!existingCategory) {
+        throw new BadRequestException('해당 카테고리가 존재하지 않습니다.');
+      }
+      if (existingCategory.subject === subject) {
+        throw new BadRequestException('변경된 내용이 없습니다.');
+      }
 
-    // target 기록 존재시 공부기록들의 카테고리를 타겟 카테고리로 수정 후 기존꺼 삭제
-    if (targetCategory) {
-      return this.mergeCategories(existingCategory, targetCategory);
-    }
-    // target 기록 없을시 기존 카테고리의 subject만 업데이트
-    return this.updateCategorySubject(existingCategory, subject);
-  }
+      const targetCategory = await this.findOne(
+        {
+          withDeleted: true,
+          where: { member: { member_id }, subject },
+        },
+        queryRunner,
+      );
 
-  private async findCategoryWithRecords(study_category_id: number) {
-    return this.studyCategoryRepository.findOne({
-      where: { study_category_id },
-      relations: { study_records: true },
-    });
-  }
-
-  private validateCategoryExistence(category: StudyCategory | null) {
-    if (!category) {
-      throw new BadRequestException('해당 카테고리가 존재하지 않습니다.');
-    }
-  }
-
-  private validateSubjectChange(category: StudyCategory, newSubject: string) {
-    if (category.subject === newSubject) {
-      throw new BadRequestException('변경된 내용이 없습니다.');
+      // target 기록 존재시 공부기록들의 카테고리를 타겟 카테고리로 수정 후 기존꺼 삭제
+      if (targetCategory) {
+        await this.mergeCategories(
+          existingCategory,
+          targetCategory,
+          queryRunner,
+        );
+        return;
+      }
+      // target 기록 없을시 기존 카테고리의 subject만 업데이트
+      await queryRunner.manager.update(
+        StudyCategory,
+        existingCategory.study_category_id,
+        { subject },
+      );
+      existingCategory.subject = subject;
+      return existingCategory;
+    } catch (e) {
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
     }
   }
 
@@ -137,36 +172,32 @@ export class StudyCategoriesService {
   private async mergeCategories(
     existingCategory: StudyCategory,
     targetCategory: StudyCategory,
+    queryRunner?: QueryRunner,
   ) {
-    // 기존 카테고리에 속한 기록의 연결을 끊고 타겟 카테고리와 연결
-    await this.studyRecordRepository.update(
-      {
-        study_record_id: In(
-          existingCategory.study_records.map((s) => s.study_record_id),
-        ),
-      },
-      {
-        study_category: { study_category_id: targetCategory.study_category_id },
-      },
-    );
+    const repository = queryRunner
+      ? queryRunner.manager.getRepository(StudyCategory)
+      : this.studyCategoryRepository;
 
-    if (targetCategory.is_hidden) {
-      targetCategory.is_hidden = false;
-      await this.studyCategoryRepository.save(targetCategory);
+    // 기존 카테고리에 속한 기록의 연결을 끊고 타겟 카테고리와 연결
+    const studyRecordIds = existingCategory.study_records.map(
+      (record) => record.study_record_id,
+    );
+    if (0 < studyRecordIds.length) {
+      await this.studyRecordsService.updateCategoryOfRecords(
+        studyRecordIds,
+        targetCategory.study_category_id,
+        queryRunner,
+      );
     }
 
-    await this.studyCategoryRepository.delete(
-      existingCategory.study_category_id,
-    );
-    return targetCategory;
-  }
+    await repository.delete(existingCategory.study_category_id);
+    if (targetCategory.deleted_at) {
+      await repository.update(targetCategory.study_category_id, {
+        deleted_at: null,
+      });
+      targetCategory.deleted_at = null;
+    }
 
-  private async updateCategorySubject(
-    category: StudyCategory,
-    newSubject: string,
-  ) {
-    category.subject = newSubject;
-    await this.studyCategoryRepository.save(category);
-    return category;
+    return targetCategory;
   }
 }
