a572efcf65bc564f421880f30bcac4142959c8b0
woo3145 <lcwoo3145@gmail.com>
2024-02-03 19:01:10 +0900

    ✨ 트랜잭션 처리(Streak)

--- apps/timer-app/src/streaks/streaks.service.ts
+++ apps/timer-app/src/streaks/streaks.service.ts
@@ -1,32 +1,47 @@
 import { StudyStreak } from '@app/database/typeorm/entities/study-streak.entity';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import {
+  DataSource,
+  FindManyOptions,
+  FindOneOptions,
+  QueryRunner,
+  Repository,
+} from 'typeorm';
 
 @Injectable()
 export class StreaksService {
   constructor(
     @InjectRepository(StudyStreak)
     private streakRepository: Repository<StudyStreak>,
+    private dataSource: DataSource,
   ) {}
 
-  async findAll(): Promise<StudyStreak[]> {
-    return this.streakRepository.find();
+  async findAll(
+    options?: FindManyOptions<StudyStreak>,
+  ): Promise<StudyStreak[]> {
+    return this.streakRepository.find(options);
   }
 
-  async findOne(id: number): Promise<StudyStreak> {
-    return this.streakRepository.findOne({ where: { study_streak_id: id } });
+  async findOne(
+    options: FindOneOptions<StudyStreak>,
+    queryRunner?: QueryRunner,
+  ): Promise<StudyStreak | null> {
+    if (queryRunner) {
+      return queryRunner.manager.findOne(StudyStreak, options);
+    }
+    return this.streakRepository.findOne(options);
   }
 
-  async findOneByMemberId(member_id: string): Promise<StudyStreak> {
-    return this.streakRepository.findOne({
-      where: { member: { member_id } },
-      relations: ['palette'],
-    });
-  }
+  async create(
+    member_id: string,
+    queryRunner?: QueryRunner,
+  ): Promise<StudyStreak> {
+    const repository = queryRunner
+      ? queryRunner.manager.getRepository(StudyStreak)
+      : this.streakRepository;
 
-  async create(member_id: string) {
-    const newStreak = this.streakRepository.create({
+    const newStreak = repository.create({
       longest_streak: 0,
       current_streak: 0,
       member: {
@@ -37,12 +52,34 @@ export class StreaksService {
     return await this.streakRepository.save(newStreak);
   }
 
-  async update(id: number, streak: Partial<StudyStreak>): Promise<StudyStreak> {
-    await this.streakRepository.update(id, streak);
-    return this.streakRepository.findOne({ where: { study_streak_id: id } });
+  async update(id: number, streak: Partial<StudyStreak>): Promise<boolean> {
+    const result = await this.streakRepository.update(id, streak);
+    return result.affected ? 0 < result.affected : false;
   }
 
   async delete(id: number): Promise<void> {
     await this.streakRepository.delete(id);
+  }
+
+  async findOrCreate(memberId: string): Promise<StudyStreak> {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      let streak = await this.findOne({
+        where: { member: { member_id: memberId } },
+      });
+      if (!streak) {
+        streak = await this.create(memberId, queryRunner);
+      }
+      await queryRunner.commitTransaction();
+      return streak;
+    } catch (e) {
+      await queryRunner.rollbackTransaction();
+      throw e;
+    } finally {
+      await queryRunner.release();
+    }
   }
 }
