b085a094aef01ac788759686b15a1bc14545097e
woo3145 <lcwoo3145@gmail.com>
2024-02-17 22:10:38 +0900

    ✨ Roles 시드 추가

---
+++ apps/timer-app/src/database/seeds/roles/roles.seed.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RoleSeedService } from './roles.seed.service';
+import { RoleEntity } from 'apps/timer-app/src/roles/entities/role.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([RoleEntity])],
+  providers: [RoleSeedService],
+  exports: [RoleSeedService],
+})
+export class RoleSeedModule {}
