52caab9a5df0460f2823a39edf4232107c21c9be
woo3145 <lcwoo3145@gmail.com>
2024-03-16 00:10:06 +0900

    ✨ 상점아이템 조회 API - 공개로 변경 (비회원 기능)

--- apps/timer-app/src/items/items.controller.ts
+++ apps/timer-app/src/items/items.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { ItemQueryDto } from './dto/item-query.dto';
+import { Public } from '../auth/decorators/public.decorator';
 
 @Controller('items')
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
 
   @Get()
+  @Public()
   async findByItemType(@Query() queryDto: ItemQueryDto) {
     const items = await this.itemsService.getShopItem(queryDto.item_type);
     return { items };
